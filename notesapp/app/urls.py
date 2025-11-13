51c800209ece3f9a4157f7993958ad1329053a29
POSTI-25 <jahnavisingh512@gmail.com>
2025-11-14 03:18:33 +0530

    dynamic notes displa

--- notesapp/app/urls.py
+++ notesapp/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app/', views.list),
+    path('app/',views.list),
+    path('app/<int:pk>/',views.detail),
 ]
--- notesapp/app/views.py
+++ notesapp/app/views.py
@@ -4,4 +4,8 @@ from .models import Notes
 # Create your views here.
 def list(request):
     all_notes = Notes.objects.all()
-    return render(request, 'app/notes_list.html', {'notes': all_notes})
+    return render(request, 'app/notes_list.html', {'app': all_notes})
+
+def detail(request, pk):
+    note = Notes.objects.get(pk=pk)
+    return render(request, 'app/notes_detail.html', {'note': note})
