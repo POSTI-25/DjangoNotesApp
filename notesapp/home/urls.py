0e189f19d1c41f6f8d55a7010e5c733b8b2f671f
POSTI-25 <jahnavisingh512@gmail.com>
2025-11-14 15:45:41 +0530

    signup logic and  page

--- notesapp/home/urls.py
+++ notesapp/home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('authorized/', views.AuthorizedViews.as_view()),
     path('login/', views.LoginInterfaceView.as_view(), name='login'),
     path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
-
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
