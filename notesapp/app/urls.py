000ed4a00378a82cc65eae8f0d3b286de6b002a7
POSTI-25 <jahnavisingh512@gmail.com>
2025-11-14 13:25:06 +0530

    styling the frontend

--- notesapp/app/urls.py
+++ notesapp/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app/',views.NotesListView.as_view()),
-    path('app/<int:pk>/',views.NotesDetailView.as_view()),
+    path('app/',views.NotesListView.as_view(), name = "notes.list"),
+    path('app/<int:pk>/',views.NotesDetailView.as_view(), name = "notes.detail"),
 ]
