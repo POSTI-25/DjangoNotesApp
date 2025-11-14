d52d440115fda7d5cafb87dd7382eaa0f469281f
POSTI-25 <jahnavisingh512@gmail.com>
2025-11-14 13:42:00 +0530

    cretinf new note frontend. using new model

--- notesapp/app/urls.py
+++ notesapp/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('app/',views.NotesListView.as_view(), name = "notes.list"),
     path('app/<int:pk>/',views.NotesDetailView.as_view(), name = "notes.detail"),
+    path('app/new', views.NotesCreateView.as_view(), name = "notes.new"),
 ]
--- notesapp/app/views.py
+++ notesapp/app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Notes
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 
 # Create your views here.
+
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title' , 'text']
+    success_url = '/smart/app/'
 
 class NotesListView(ListView):
     model = Notes
