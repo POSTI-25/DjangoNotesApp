3da53bfd6097f8619775c997bd67467682f86b49
POSTI-25 <jahnavisingh512@gmail.com>
2025-11-14 15:03:00 +0530

    new model for users

--- notesapp/app/views.py
+++ notesapp/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Notes
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -21,9 +22,13 @@ class NotesCreateView(CreateView):
     success_url = '/smart/app/'
     form_class = NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
-    context_object_name = 'app'
+    context_object_name = "notes"
+    login_url = "/admin/login/"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
